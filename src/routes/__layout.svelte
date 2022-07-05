a8c35fff8cb10d0b27348aabb3c8ddc180e39afe
gabrielss97 <gabriel.ssalvador97@gmail.com>
2022-07-05 12:47:47 -0300

    bg adjust

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -10,7 +10,7 @@ import Header from '../components/Header.svelte';
 
 
 <div class="py-8 px-2 relative">
-    <div class="bg-bg-binary bg-no-repeat bg-cover min-h-screen -z-10 absolute opacity-[.01] w-full top-0 left-0"></div>
+    <div class="bg-[url('./assets/bg-binary.jpg')] bg-no-repeat bg-cover min-h-screen -z-10 absolute opacity-[.02] w-full top-0 left-0"></div>
     <Nav />
     <Header/>
 </div>
