d45af02c3c63a35afe79802734a6b1a612d3517a
gabrielss97 <gabriel.ssalvador97@gmail.com>
2022-07-06 20:29:48 -0300

    finish feedbacks start tecs

---
+++ src/components/Tecs.svelte
@@ -0,0 +1,17 @@
+<div class="flex justify-center">
+    <h1 class="text-4xl text-center font-bold uppercase bg-cyellow text-cblack mb-4 px-2 rounded desktop:text-6xl desktop:mb-10">O QUE VOCÊ VAI APRENDER?</h1>
+    <div class="flex flex-wrap">
+        <div class="html">
+            
+        </div>
+        <div class="css-sass"></div>
+        <div class="flex-grid"></div>
+        <div class="javascript"></div>
+        <div class="responsividade"></div>
+        <div class="bootstrap"></div>
+        <div class="api"></div>
+        <div class="react"></div>
+        <div class="github"></div>
+        <div class="linkedin"></div>
+    </div>
+</div>
