6be63d373cf775535e479a3665cd206eed496302
gabrielss97 <gabriel.ssalvador97@gmail.com>
2022-07-16 18:51:30 -0300

    new Sale

--- src/components/Sale.svelte
+++ src/components/Sale.svelte
@@ -1,25 +1,30 @@
 <script>
 	import MainButton from './MainButton.svelte';
-	import DevGirl from '../assets/dev-girl.jpg';
+	import DevGirl from '../assets/dev-girl-removebg.png';
+
+    export let buttonText = 'INSCREVA-SE AGORA MESMO';
+	export let borderColor = 'border-[#00DFC0]'
+	export let href =
+		'https://pay.hotmart.com/G70568619V?bid=1657111023469';
 </script>
 
 <div class="flex flex-col items-center" id="investimento">
 	<h1
-		class="text-4xl text-center font-bold uppercase bg-cyellow text-cblack mb-4 px-2 rounded laptop:mb-8 desktop:text-6xl desktop:mb-10"
+		class="text-4xl text-center font-bold uppercase bg-[#00DFC0] cyellow text-cblack mb-4 px-2 rounded laptop:mb-8 desktop:text-6xl desktop:mb-10"
 	>
 		Investimento
 	</h1>
 	<div  class="flex flex-col items-center laptop:flex-row laptop:px-4 laptop:justify-center">
         <img src={DevGirl} alt="" class="max-w-[90vw] rounded mb-4 laptop:max-w-[30%]">
 		<div class="flex flex-col justify-center">
-			<p class="mb-10 text-xl tablet:mx-10 laptop:text-2xl max-w-lg desktop:text-4xl">
-				Priorize sua carreira, investir no seu futuro vai sair bem mais barato do que você imagina,
+			<p class="mb-10 text-xl tablet:mx-10 laptop:text-2xl max-w-lg desktop:text-3xl">
+				Priorize sua <span class="text-[#00DFC0]">carreira</span>, investir no seu futuro vai sair bem mais <span class="text-[#00DFC0]">barato</span> do que você imagina,
 				por apenas:
 			</p>
 			<div class="flex justify-center gap-3 mx-auto pb-2 tablet:mx-auto laptop:w-[30vw]">
 				<span class="leading-6 text-2xl text-[#bec9d3]">12x</span>
 				<span
-					class="leading-[70px] text-7xl font-bold border-b-4 border-cyellow pb-2 desktop:text-8xl"
+					class="leading-[70px] text-7xl font-bold border-b-4 border-[#00DFC0] pb-2 desktop:text-8xl"
 					>116</span
 				>
 				<div class="flex flex-col gap-2">
@@ -30,7 +35,10 @@
 		</div>
 	</div>
 	<div class="mt-10 flex justify-center min-w-full">
-		<MainButton />
+		<button
+	class=" hover:text-cblack hover:bg-[#00DFC0] tablet:block rounded-2xl py-2 px-5 text-cwhite {borderColor} border-2 font-bold text-center desktop:w-1/4 desktop:text-2xl desktop:mt-4 desktop:border-4 laptop:text-2xl laptop:px-16 desktop:px-5"
+	><a target="_blank" {href}>{buttonText}</a></button
+>
 	</div>
 </div>
 
@@ -38,6 +46,7 @@
 	p {
 		font-weight: 400;
 		line-height: 34px;
-		color: #bec9d3;
+        text-align: justify;
+		color: white;
 	}
 </style>
