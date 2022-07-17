20f874f0c8ca1d448c347ca1bdd39d3717356174
gabrielss97 <gabriel.ssalvador97@gmail.com>
2022-07-16 23:47:07 -0300

    ajuste benefits

--- src/components/Benefits.svelte
+++ src/components/Benefits.svelte
@@ -1,30 +1,30 @@
 <div class="w-full flex justify-center gap-10 flex-wrap desktop:px-40">
 	<div class="benefits-wrapper">
-		<i class="fa-4x fa-solid fa-infinity text-cyellow" />
+		<i class="fa-3x fa-solid fa-infinity text-cyellow" />
 		<p class="benefits-text">Acesso <br /> Vitalício</p>
 	</div>
 	<div class="benefits-wrapper">
-		<i class="fa-4x fa-solid fa-clock text-cyellow" />
+		<i class="fa-3x fa-solid fa-clock text-cyellow" />
 		<p class="benefits-text">+ de 30 horas <br /> de contéudo</p>
 	</div>
 	<div class="benefits-wrapper">
-		<i class="fa-4x fa-solid fa-medal text-cyellow" />
+		<i class="fa-3x fa-solid fa-medal text-cyellow" />
 		<p class="benefits-text">Garantia <br /> de 7 dias</p>
 	</div>
 	<div class="benefits-wrapper">
-		<i class="fa-4x fa-solid fa-certificate text-cyellow" />
+		<i class="fa-3x fa-solid fa-certificate text-cyellow" />
 		<p class="benefits-text">Emissão de <br /> Certificado</p>
 	</div>
 	<div class="benefits-wrapper">
-		<i class="fa-4x fa-solid fa-hand-holding-heart text-cyellow" />
+		<i class="fa-3x fa-solid fa-hand-holding-heart text-cyellow" />
 		<p class="benefits-text">Suporte pelo <br /> WhatsApp</p>
 	</div>
 	<div class="benefits-wrapper">
-		<i class="fa-4x fa-solid fa-people-group text-cyellow" />
+		<i class="fa-3x fa-solid fa-people-group text-cyellow" />
 		<p class="benefits-text">+ de 300 <br /> alunos(as)</p>
 	</div>
 	<div class="benefits-wrapper">
-		<i class="fa-4x fa-solid fa-record-vinyl text-cyellow" />
+		<i class="fa-3x fa-solid fa-record-vinyl text-cyellow" />
 		<p class="benefits-text ">Todas as <br /> aulas ao-vivo</p>
 	</div>
 </div>
