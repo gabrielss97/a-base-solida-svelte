b2074c6084c6f19f4231813a1156fa5b30c6cc2a
gabrielss97 <gabriel.ssalvador97@gmail.com>
2022-07-17 11:21:20 -0300

    teste

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -5,13 +5,13 @@
 
 <header class="flex flex-col gap-8 items-center laptop:pl-4 desktop:px-40">
 	<div class="flex flex-col gap-8 laptop:flex-row desktop:h-[450px]">
-		<iframe defer
+		<!-- <iframe defer
 			class="w-full h-72 tablet:px-10 laptop:flex-1 laptop:px-0 desktop:h-full rounded"
 			src={mainVideo}
 			title="Teste"
 			frameborder="0"
 			allowfullscreen
-		/>
+		/> -->
 		<div
 			class="tablet:px-10 laptop:w-1/2 laptop:px-6 desktop:flex desktop:flex-col desktop:justify-around"
 		>
