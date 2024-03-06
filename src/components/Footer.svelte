6b207d41389bc3707a584c281ba80364139d837d
gabrielss97 <gabriel.ssalvador97@gmail.com>
2024-03-06 17:56:45 -0300

    ajuste footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -5,10 +5,11 @@
 <footer
 	class="flex flex-col justify-center gap-4 items-center py-4 px-4 tablet:justify-between desktop:px-40 bg-[#171719]"
 >
-	
-	<div class="flex flex-col gap-8 tablet:flex-row desktop:gap-20 laptop:gap-4 tablet:justify-between desktop:px-40 items-center w-[90vw]">
-        <img src={logo} alt="logo" class="w-12 rounded" />
-        
+	<div
+		class="flex flex-col gap-8 tablet:flex-row desktop:gap-20 laptop:gap-4 tablet:justify-between desktop:px-40 items-center w-[90vw]"
+	>
+		<img src={logo} alt="logo" class="w-12 rounded" />
+
 		<ul class="hidden laptop:flex text-cgray gap-5 desktop:pl-48">
 			<li class="hover:text-cwhite"><a href="#tecs">O que você vai aprender?</a></li>
 			<li class="hover:text-cwhite"><a href="#feedbacks">Feedbacks</a></li>
@@ -18,6 +19,14 @@
 			<li class="hover:text-cwhite"><a href="#faq">FAQ</a></li>
 		</ul>
 	</div>
-    
-    <p class="text-cgray text-center py-4">Criado com ❤️ por Gabriel Salvador © 2022</p>
+
+	<div>
+		<p class="text-cgray text-center py-4">
+			Email: suporte@frontbase.com.br <br />
+			CNPJ: 45.475.703/0001-04 <br />
+			Rua Wilson Barbosa de Melo, 135 <br />
+			CEP. 49037590 - Atalaia, Aracaju - SE <br />
+			Criado com ❤️ por Gabriel Salvador © 2022
+		</p>
+	</div>
 </footer>
