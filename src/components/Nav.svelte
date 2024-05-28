76c454cb28f717f7ebebed52e34497c9e8385c57
gabrielss97 <gabriel.ssalvador97@gmail.com>
2024-05-28 15:53:35 -0300

    ajuste nav

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -17,13 +17,5 @@
 			<li class="hover:text-cwhite"><a href="#investimento">Investimento</a></li>
 			<li class="hover:text-cwhite"><a href="#faq">FAQ</a></li>
 		</ul>
-		<button
-			class="hidden hover:bg-cgolden bg-cyellow hover:border-cgolden text-cblack tablet:block rounded py-2 px-5 border-cyellow border-2 font-bold"
-			><a
-				target="_blank"
-				href="https://www.mercadopago.com.br/checkout/v1/payment/redirect/92003108-948c-44b4-85c4-857989c90725/payment-option-form/?source=link&preference-id=282933471-ea9b0b27-162a-4034-b6b6-6e60bda44545&sniffing-rollout=sniffing-api&router-request-id=eb9d1422-4424-4014-9538-eda16e178e05&p=11040522b74b979f8f204e99c61b88f6#/"
-				>INSCREVA-SE AGORA</a
-			></button
-		>
 	</div>
 </nav>
